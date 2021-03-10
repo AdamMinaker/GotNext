71d204bc54466d98b318c34b1e8c980902be5a0d
Adam <adamminaker@outlook.com>
2021-03-10 15:25:08 -0600

    Added Games and Locations table PDO to index.php

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
 <!--
   Author: Adam Minaker
-  Date: 3/4/2021
+  Date: 3/10/2021
   Description: A medium for basketball players to organize games on public courts.
 -->
 <?php
   require 'connect.php';
+
+  // Query the db for all active games.
+  $query = "SELECT games.GameID, locations.LocationID, games.Description, games.Duration, games.PostedAt, locations.Name, locations.Image
+            FROM games
+            JOIN locations ON locations.LocationID = games.LocationID";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $games = $statement->fetchAll();
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>GotNext</title>
-  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <style>
-    .bd-placeholder-img {
-      font-size: 1.125rem;
-      text-anchor: middle;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      user-select: none;
-    }
-
-    @media (min-width: 768px) {
-      .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-      }
-    }
-  </style>
+  <link href="css/bootstrap.min.css?v1.0" rel="stylesheet">
 </head>
 <body>
     
@@ -37,14 +30,12 @@
       <div class="container">
         <div class="row">
           <div class="col-sm-8 col-md-7 py-4">
-            <h4 class="text-white">Hi there,</h4>
-            <p class="text-white">I created this to provide a medium for basketball players to organize games on public courts.</p>
+            <p class="text-white">A medium for basketball players to organize games on public courts.</p>
           </div>
           <div class="col-sm-4 offset-md-1 py-4">
             <ul class="list-unstyled">
-              <li><a href="#" class="text-white">Games</a></li>
-              <li><a href="#" class="text-white">Locations</a></li>
-              <li><a href="#" class="text-white">Report an issue</a></li>
+              <li><a href="#" class="text-white">Current Games</a></li>
+              <li><a href="#" class="text-white">Court Locations</a></li>
             </ul>
           </div>
         </div>
@@ -52,9 +43,9 @@
     </div>
     <div class="navbar navbar-dark bg-dark shadow-sm">
       <div class="container">
-        <a href="#" class="navbar-brand d-flex align-items-center">
+        <a href="index.php" class="navbar-brand d-flex align-items-center">
           <img src="GotNext.png" alt="GotNext Logo" width="30" height="30">
-          <strong style="padding-left: 10px;">GotNext</strong>
+          <strong style="padding-left: 10px; color: #EA5455">GotNext</strong>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -71,7 +62,7 @@
           <h1 class="fw-light">Games</h1>
           <p class="lead text-muted">Games happening right now</p>
           <p>
-            <a href="#" class="btn btn-primary my-2">Create Game</a>
+            <a href="#" class="btn btn-primary my-2">New Game</a>
           </p>
         </div>
       </div>
@@ -81,71 +72,41 @@
       <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+          <!-- PHP card -->
+          <?php foreach ($games as $game): ?>
+            <div class="col">
+              <div class="card shadow-sm">
+                <img width ="100%" height="225" src="<?=$game['Image']?>">
+                <div class="card-body">
+                  <h5 class="card-title"><?=$game['Name']?></h5>
+                  <p class="card-text"><?=$game['Description']?></p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <small class="text-muted">8/10 Players</small>
+                    <progress value="0.85"></progress>
+                    <small class="text-muted">9 mins remaining</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <?php endforeach ?>
+          
+          <!-- Template card 
           <div class="col">
             <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+              <img width ="100%" height="225" src="image/court.jpg">
               <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">Warren Park</p>
                 <div class="d-flex justify-content-between align-items-center">
                   <small class="text-muted">8/10 Players</small>
-                  <progress value="0.4"></progress>
+                  <progress value="0.85"></progress>
                   <small class="text-muted">9 mins remaining</small>
                 </div>
               </div>
             </div>
-          </div>
+          </div> 
+          -->
           
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>       
-        
         </div>
       </div>
     </div>
